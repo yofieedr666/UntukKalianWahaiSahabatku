8ea9292bb7dc6df0381eef7390e8301c6fb7be34
yofieedr666 <yofiemadridista@gmail.com>
2023-06-17 17:22:03 +0800

    UntukDuniaSementara

---
+++ SahabatProgram.java
@@ -0,0 +1,27 @@
+public class SahabatProgram {
+    public static void main(String[] args) {
+        System.out.println("Kita selalu bersatu setiap langkah");
+        System.out.println("Beriringan menuntut ilmu");
+        System.out.println("Mengarungi samudra menuju sukses di masa depan");
+        System.out.println("Sahabat,");
+        System.out.println("Menemuimu di tempat ini");
+        System.out.println("adalah anugerah bagiku");
+        System.out.println("Kau anugerah terindah yang kumiliki");
+        System.out.println("Harapku selalu,");
+        System.out.println("Kita jadi manusia yang baik nanti");
+        System.out.println("Sahabat, apakah kita salah?");
+        System.out.println("Jika kita merangkai waktu");
+        System.out.println("Menjadi indah dan berkilau");
+        System.out.println("Dengan anyaman kebersamaan");
+        System.out.println("Hingga kelulusan menghampiri kita");
+        System.out.println("Kau yang mengajariku");
+        System.out.println("Kau adalah guru terbaik dalam masa sekolahku");
+        System.out.println("Dengan rasa, aku haturkan terima kasih");
+        System.out.println("Kuhantarkan doa");
+        System.out.println("Semoga Tuhan selalu menyertaimu");
+        System.out.println("Terima kasih sahabat,");
+        System.out.println("Tanpamu aku hanya bisa berdoa");
+        System.out.println("Tanpa arti, tanpa makna");
+        System.out.println("Terima kasih sahabat");
+    }
+}
